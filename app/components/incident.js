d61fa943dfd55a1ba9a5b4f636769906c56d9a65
fernandogelin <fernandogelin@gmail.com>
2019-10-17 18:37:39 -0400

    feat: add history route

---
+++ app/components/incident.js
@@ -0,0 +1,22 @@
+import Component from '@glimmer/component';
+
+export default class IncidentComponent extends Component {
+  get incidentType() {
+    let incidentType
+    this.args.issues.map((issues) => {
+      issues.labels.map((label) => {
+        console.log(label)
+        if (label.name === 'scheduled maintanance') {
+          incidentType = 'scheduled'
+        }
+        if (label.name === 'partial disruption') {
+          incidentType = 'partial'
+        }
+        if (label.name === 'full disruption') {
+          incidentType = 'full'
+        }
+      })
+    })
+    return incidentType
+  }
+}
--- app/components/repo-box.js
+++ app/components/repo-box.js
@@ -2,9 +2,36 @@ import Component from '@glimmer/component';
 
 export default class RepoBoxComponent extends Component {
   get statusClass() {
-    const statusClass = (this.args.open > 0)
-    ? { border: 'border-danger', fa: 'times', faColor: 'color-danger' }
-    : { border: 'border-success', fa: 'check', faColor: 'color-success' }
+    let statusClass
+    if (this.args.open === 0) {
+      statusClass = { border: 'border-success',
+      fa: 'check',
+      faColor: 'color-success',
+      text: 'Operational' }
+    }
+
+    this.args.issues.map((issue) => {
+      issue.labels.map((label) => {
+        if (label.name === 'scheduled maintanance') {
+          statusClass = { border: 'border-primary',
+          fa: 'clock',
+          faColor: 'color-primary',
+          text: 'Scheduled Maintanance' }
+        }
+        if (label.name === 'partial disruption') {
+          statusClass = { border: 'border-warning',
+          fa: 'exclamation',
+          faColor: 'color-warning',
+          text: 'Partial Disruption' }
+        }
+        if (label.name === 'full disruption') {
+          statusClass = { border: 'border-danger',
+          fa: 'times',
+          faColor: 'color-danger',
+          text: 'Full Disruption' }
+        }
+      })
+    })
 
     return statusClass
   }
