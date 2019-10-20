e88590dda7a335cf9e78f88de6368774566d3f87
fernandogelin <fernandogelin@gmail.com>
2019-10-20 13:29:07 -0400

    ci: test deploy

--- config/environment.js
+++ config/environment.js
@@ -49,6 +49,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.baseURL = '/ccv-ember-frontend/'
     // here you can enable a production-specific feature
   }
 
